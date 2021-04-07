d85c60a8dc4f6b01b39393ade7a6e6f2233fce57
Corey Mundy <minusemerkat3@yahoo.com>
2021-04-07 13:20:49 -0600

    Added FPS counter
    We can now change the fps and see what the fps is

--- Assets/Scripts/Engine/World.cs
+++ Assets/Scripts/Engine/World.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class World : MonoBehaviour
 {
     public BoolData simulate;
     public FloatData gravity;
+    public FloatData fixedFPS;
+    public TMP_Text valueText = null;
 
     static World instance;
+
+    public float timeAccumaltor;
+
+    public float fixedDeltaTime { get { return 1.0f / fixedFPS.value; } }
     static public World Instance { get { return instance; } }
 
     public Vector2 Gravity { get { return new Vector2(0, gravity.value); } }
@@ -20,16 +27,28 @@ public class World : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(1.0f / Time.deltaTime);
         if (!simulate.value)
         {
             return;
         }
         float dt = Time.deltaTime;
 
+        timeAccumaltor += Time.deltaTime;
+       
         bodies.ForEach(body => body.Step(dt));
         bodies.ForEach(body => Intergrator.SemiEular(body, dt));//ExplicitEuler(body, dt));
 
         bodies.ForEach(body => body.force = Vector2.zero);
         bodies.ForEach(body => body.acceleration = Vector2.zero);
+
+
+        while (timeAccumaltor > fixedDeltaTime) 
+        { 
+            bodies.ForEach(body => body.Step(fixedDeltaTime)); 
+            bodies.ForEach(body => Intergrator.ExplicitEuler(body, fixedDeltaTime)); 
+            timeAccumaltor = timeAccumaltor - fixedDeltaTime; 
+        }
+        valueText.text = fixedFPS.value.ToString("F2");
     }
 }
